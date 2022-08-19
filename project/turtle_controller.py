437040059691bcf69130412fc90553a91b2778c9
mahmud10cool <mutashi@gmail.com>
2022-08-19 12:01:41 +0600

    Finished the P controller design

--- project/turtle_controller.py
+++ project/turtle_controller.py
@@ -9,82 +9,76 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 # Importing the Pose message type for pose subscription
 from turtlesim.msg import Pose
+# Importing the math module for pythagorean theorem
+import math
 
 
 class TurtleControllerNode(Node):
     def __init__(self):
+        # Naming the node
         super().__init__('turtle_controller')
+        self.target_x = 8.0
+        self.target_y = 4.0
 
-    # What do we have to do first?
-    # What is the function of this node?
-    # To control the turtle
-    # Which means it will need to publish things to the turtlesim node
-    # So I have to create a publisher to the turtlesim node
-    # But before that I need to find the name of the topic
-    # The topic for the velocity is /turtle1/cmd_vel
+        # A variable for the pose
+        self.pose_ = None
+        # Creating a velocity publisher
+        self.velocity_publisher_ = self.create_publisher(
+            Twist, '/turtle1/cmd_vel', 10)
 
-    # Let us create a publisher that will publish to that topic
-    # The arguments that go inside the create_publisher function are what?
-    # The first argument will be the type of message that is published to the topic
-    # What is that? Let us find out ...
-    # It is a Twist type of message found in the geometry_msgs folder
-    # The second argument will be the topic name 
-    # The third argument is the queue size
-    # What is the queue size?
-
-
-        self.velocity_publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        # Creating a subscriber
+        self.pose_subscriber_ = self.create_subscription(
+            Pose, '/turtle1/pose', self.subscriber_callback, 10)
         
-    # Next we have to create a timer for the publisher
-    # To determine how frequently it should publish
-    # But what are the arguments that go into the create_timer function???
-    # Two things: 1. The timer period, 2. A callback function
+        # Frequency of the publisher
+        timer_period = 0.01
 
-    # Let's set the time period to something small so the velocity is regularly published
-    # Let us also see what happens if we publish very infrequently
-    # Answer: It's not a good idea for this project
-    # I'll leave it to be 0.1 for now unless I need to make it slower
-        timer_period = 0.1
+        # Creating the timer
+        self.control_loop_timer_ = self.create_timer(timer_period, self.control_loop)
 
-        self.velocity_timer_ = self.create_timer(timer_period, self.timer_callback)
-
-    # Let us write an intializing message
-
-        self.get_logger().info('The velocity publisher has initialized ...')
-
-    # Now this node must also have a subscriber to know the position of the turtle at any given time
-    # This can be known by subscribing to the turtle1/pose topic
-    # It uses the pose type of message from the turtlesim library
-    # But what are the arguments that go into a create_subscription function
-    # Three things: 1. Type of message, 2. Topic name, 3. A callback function
-
-        self.create_subscription(Pose, '/turtle1/pose', self.subscriber_callback, 10)
-
-    def timer_callback(self):
-        # In this function we will assign what to publish in the time period given
-        # Twist is a message type with two vectors; one for linear and one for angular velocity
-        vel = Twist()
-        # For the linear velocity
-        vel.linear.x = 1.0
-        vel.linear.y = 0.0
-        vel.linear.z = 0.0
-        # For the angular velocity
-        vel.angular.x = 0.0
-        vel.angular.y = 0.0
-        vel.angular.z = 1.0
-
-        self.velocity_publisher_.publish(vel)
+        # Let us write an intializing message
+        self.get_logger().info('The turtle controller has now started')
 
     def subscriber_callback(self, hello):
-        pose = hello
-        self.x_pose = pose.x
-        self.y_pose = pose.y
-        self.theta_pose = pose.theta
+        self.pose_ = hello
+
+    def control_loop(self):
+        if self.pose_ == None:
+            return 
+
+        dist_x = self.target_x - self.pose_.x
+        dist_y = self.target_y - self.pose_.y
+
+        distance = math.sqrt(dist_x ** 2 + dist_y ** 2)
+
+        msg = Twist()
+
+        if distance > 0.5:
+            # First the position
+            msg.linear.x = 2*distance
+
+            # The orientation
+            goal_theta = math.atan2(dist_y, dist_x)
+            diff = goal_theta - self.pose_.theta
+            if diff > math.pi:
+                diff -= 2*math.pi
+            elif diff < -math.pi:
+                diff += 2*math.pi
+
+            msg.angular.z = 6*diff
+        else:
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+
+        self.velocity_publisher_.publish(msg)
 
 
+# Defining the main function
 def main(args=None):
+    # Initializing ROS
     rclpy.init(args=args)
     node = TurtleControllerNode()
+    # Spinning the node
     rclpy.spin(node)
     rclpy.shutdown()
 
